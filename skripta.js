98e430935515a95886ee5bde251e355a0264eb0f
Ardit Demolli <n_arditi_98@hotmail.com>
2021-02-19 14:41:33 +0100

    Ndryshime bazike ne header, main etj.

---
+++ skripta.js
@@ -0,0 +1,11 @@
+var divElements = document.getElementsByClassName('slider-content');
+var sliderIndex	= 0;
+document.getElementsByClassName('main-top')[0].addEventListener('click', function (event){
+	divElements[sliderIndex].classList.remove('active')
+	divElements[sliderIndex].classList.add('not-active')
+
+	sliderIndex++;
+	if (sliderIndex == divElements.length) sliderIndex = 0;
+	divElements[sliderIndex].classList.add('active')
+	divElements[sliderIndex].classList.remove('not-active')
+})
